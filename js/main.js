b36de5ac083cb7a21d20d15ff02598a2425febc5
MohammedEl-sayedAhmed <mohammedelsayed4648@gmail.com>
2024-06-24 23:30:00 +0300

    Add typed string effect with comments

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function initializeTypingEffect() 
+{
+    var options = {
+        strings: ["I am Mohammed", "I am a Freelancer", "I am a developer"],
+        typeSpeed: 70,
+        backSpeed: 50,
+        backDelay: 1500,
+        startDelay: 1000,
+        loop: true
+    };
+    var typed = new Typed(".typed-output", options);
+}
+
+document.addEventListener("DOMContentLoaded", initializeTypingEffect);
+
+
+
