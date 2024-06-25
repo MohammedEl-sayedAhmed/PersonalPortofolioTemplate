33b2e237a49e50ebe39e31df04b6105ddfc63d11
MohammedEl-sayedAhmed <mohammedelsayed4648@gmail.com>
2024-06-25 04:13:59 +0300

    add comments for js file

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
+// Function to initialize the typing effect
 function initializeTypingEffect() 
 {
+    // Configuration options for the typing effect
     var options = {
+        // Array of strings to type out
         strings: ["I am Mohammed Elsayed", "I am a Freelancer.", "I am a Developer."],
+        // Speed of typing (in milliseconds)
         typeSpeed: 70,
+        // Speed of backspacing (in milliseconds)
         backSpeed: 50,
+        // Delay before starting to backspace (in milliseconds)
         backDelay: 1500,
+        // Delay before starting to type (in milliseconds)
         startDelay: 1000,
+        // Loop the typing effect
         loop: true
     };
+
+    // Create a new instance of the Typed object and apply it to the element with class "typed-output"
     var typed = new Typed(".typed-output", options);
 }
 
+// Event listener to initialize the typing effect when the DOM content is fully loaded
 document.addEventListener("DOMContentLoaded", initializeTypingEffect);
